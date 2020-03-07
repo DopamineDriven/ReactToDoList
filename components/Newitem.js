83334b4611f951c9fd55cbe4e67b5d20bd8cfaa1
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-03-07 04:05:38 -0600

    item addition functioning

--- components/Newitem.js
+++ components/Newitem.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
 export default class Newitem extends React.Component{
+    constructor(){
+        super();
+
+        this.state={
+            text: ''
+        }
+        // pre-binds preventing from having to bind multiple times
+        this.updateText = this.updateText.bind(this)
+    }
+
+    updateText(e) {
+        this.setState({
+            text: e.target.value
+        })
+    }
+
+
     render(){
         return(
             <div>
-                <input type="text"/>
-                    <button>
-                        Add Item
-                    </button>
+                <input 
+                    onChange={this.updateText}
+                    type="text"/>
+                <button
+                    onClick={()=>{this.props.addItem(this.state.text)}}
+                >Add Item</button>
             </div>
         )
     }
--- components/ToDoApp.js
+++ components/ToDoApp.js
@@ -12,12 +12,23 @@ export default class ToDoApp extends React.Component{
                 "code a website"
             ]
         }
+        this.addItem = this.addItem.bind(this)
     }
+    // when addItem function is claled we want to update state of list
+    addItem(item) {
+        let newList = this.state.list
+        newList.push(item)
+        this.setState({
+            list: newList
+        })
+    }
+
+
     render(){
         return(
             <div>
                 <h3>ToDo List App</h3>
-                <Newitem />
+                <Newitem addItem={this.addItem}/>
                 <ul>
                     {this.state.list.map((item) => {
                         return <ToDo item={item}/>
